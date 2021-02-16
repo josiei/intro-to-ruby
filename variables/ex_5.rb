e0b16e24a9762c400ca3463b1bd94d86afa9fa31
Jocelyn Icaza <jocelyn.icaza@gmail.com>
2021-02-16 00:31:56 +0000

    variable exercises

---
+++ variables/ex_5.rb
@@ -0,0 +1,2 @@
+# 1. x = 3
+# 2. x would equal 3, but throws a scope error instead
---
+++ variables/name.rb
@@ -0,0 +1,6 @@
+puts "What is your first name?"
+first = gets.chomp
+puts "What is your last name?"
+last = gets.chomp
+
+puts first + " " + last
